0bbfd721929f82889d9833a4b1cec2049e4dca92
CodeDiary18 <74449232+CodeDiary18@users.noreply.github.com>
2020-12-16 23:09:48 +0900

    Create 11021.cpp

---
+++ 3. for문/11021.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 1;i <= a;i++) {
+		int c1, c2;
+		cin >> c1 >> c2;
+		cout <<"Case #"<<i<<": "<< c1 + c2<<"\n";
+	}
+	
+	return 0;
+}
