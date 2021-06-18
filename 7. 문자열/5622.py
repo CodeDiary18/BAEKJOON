aebf7463e2538695ddea95f9dc4c943e6ee44011
CodeDiary18 <codediary18@gmail.com>
2021-06-19 02:28:12 +0900

    Create 5622.py

---
+++ 7. 문자열/5622.py
@@ -0,0 +1,8 @@
+word = input()
+dial = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+result=0
+for i in range(len(word)):
+    for j in dial:
+        if word[i] in j:
+            result+=dial.index(j)+3
+print(result)
